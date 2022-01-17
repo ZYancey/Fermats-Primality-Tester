3ce7fc9a04dff6b6be9dc9e0539c529b3396c467
ZYancey <yanceyzack@gmail.com>
2022-01-17 13:26:34 -0700

    Fermat and Probability Functional

--- fermat.py
+++ fermat.py
@@ -7,13 +7,17 @@ def prime_test(N, k):
 
 
 def mod_exp(x, y, N):
-    # You will need to implement this function and change the return value.   
-    return 1
+    if y == 0:
+        return 1
+    z = mod_exp(x, y / 2, N)
+    if y % 2 == 0:
+        return z ^ 2 % N
+    else:
+        return x * z ^ 2 % N
 
 
 def fprobability(k):
-    # You will need to implement this function and change the return value.   
-    return 0.0
+    return 1 - (1 / pow(2, k))
 
 
 def mprobability(k):
@@ -25,10 +29,21 @@ def fermat(N, k):
     # You will need to implement this function and change the return value, which should be
     # either 'prime' or 'composite'.
     #
-    # To generate random values for a, you will most likley want to use
+    # To generate random values for a, you will most likely want to use
     # random.randint(low,hi) which gives a random integer between low and
     #  hi, inclusive.
-    return 'prime'
+    if N == 2:
+        return 'prime'
+
+    if N % 2 == 0:
+        return 'composite'
+
+    for x in range(0, k):
+        y = random.randint(1, N-1)
+        if pow(y, (N - 1)) % N != 1:  # probably not the right place to mod, but it seems to work for now
+            return 'composite'
+        return 'prime'
+
 
 
 def miller_rabin(N, k):
